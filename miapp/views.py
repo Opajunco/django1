386f868c097ffe63cd8d8b768624cd530c5c1c2d
Francisco Jerez <info@franciscojerez.com>
2024-01-07 23:12:02 +0100

    segunda actualización de prueba

--- miapp/views.py
+++ miapp/views.py
@@ -1,8 +1,17 @@
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
 
 def hola_mundo(request):
         return HttpResponse("Holaaaaa Maríaaa! :)")
+
+def index(request):
+    lenguajes= ['Python','Nodejs', 'PHP']
+    return render(request, 'index.html',{
+        'mi_variable':'Soy un dato que está en la vista',
+         'nombre':'Franulo',
+        'languages' : lenguajes, #lista definida anteriormente
+        'titulo' : 'inicio(variable)'
+    })
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+#mis vistas
 from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hola_mundo, name='helloWorld')
+    path('', views.index, name='index'), #como he importado views anteriormente no tengo que poner miapp
 ]
